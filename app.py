1b00a6b84445ef822aedd6a71c47ef3a0f3b6475
Alejandro Montes <aaleemd11@gmail.com>
2021-06-15 20:50:39 +0200

    corregido los errores

--- app.py
+++ app.py
@@ -1,26 +1,20 @@
-from flask import Flask, render_template,request,abort
-import json
+from flask import Flask, render_template,request,abort,json
 import os
 import sys
 app = Flask(__name__)
-def leer_json(fichero):
-    try:
-        with open(fichero) as f:
-            datos=json.load(f)
-            return datos
-    except:
-        print("ERROR AL LEER EL FICHERO")
-        sys.exit(0)
-libros=leer_json("books.json")
+datos = open('books.json')
+libros = json.load(datos)
 
-@app.route('/')
+@app.route('/',methods=["GET","POST"])
 def inicio():
 	return render_template("base.html",libreria=libros)
+
 @app.route('/libros/<isbn>')
 def saca_isbn(isbn):
     for libro in libros:
         if libro.get("isbn")==isbn:
             return render_template("libros.html",almacen=libro)
-        abort(404)
+    abort(404)    
+
 #port=os.environ["PORT"]
 app.run("0.0.0.0",port=5555,debug=True)
